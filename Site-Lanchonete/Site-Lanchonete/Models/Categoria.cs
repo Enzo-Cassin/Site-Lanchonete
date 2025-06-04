a2c4136afb3a9a3b367bfa28001e0dc60ea34e42
Enzo Cassin <cassinenzo@yahoo.com.br>
2025-06-04 14:55:38 -0300

    Implementação da área administrativa, possibilidade de criar, editar e/ou excluir lanches, categorias e pedidos da base de dados

--- Site-Lanchonete/Site-Lanchonete/Models/Categoria.cs
+++ Site-Lanchonete/Site-Lanchonete/Models/Categoria.cs
@@ -16,7 +16,7 @@ namespace Site_Lanchonete.Models
 
         [StringLength(200, ErrorMessage = "O tamanho máximo são 200 caracteres")]
         [Required(ErrorMessage = "Informe a descrição da categoria")]
-        [Display(Name = "Nome")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
